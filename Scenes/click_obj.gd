2b2fee1537d0939444e64cb0c89f475961bef0d0
studentXxxxNagibatorxxX <arem125@yandex.ru>
2024-10-16 15:40:30 +0300

    new

--- Scenes/click_obj.gd
+++ Scenes/click_obj.gd
@@ -1,14 +1,55 @@
 extends Area2D
 
-var n = 1
+# кол во кликов
+var clik = 0
+# мощ клика по умолчанию 1
+var clikStr = 1
+# цена апгрейда
+var costUpgrade = 10
 
 
-
+func _process(delta):
+	$ScoreLable.text = str(clik)
+	
+	
+	
+	
+# функция тапанья
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:		
-		print(n)
+	if event is InputEventMouseButton and event.pressed and event.button_index != MOUSE_BUTTON_WHEEL_UP and event.button_index != MOUSE_BUTTON_WHEEL_DOWN:	
+		print(clik)
 		
-		$ScoreLable.text = "Твой счёт: " + str(n)
+		#$ScoreLable.text = str(clik)
 		for i in range(1):
-			n = n + 1
+			clik = clik + clikStr
 			
+
+
+
+
+# функция апгрейда
+func _on_button_button_down() -> void:
+	if clik >= costUpgrade:
+		clik = clik - costUpgrade
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		

6a40bcffba7ce2b7c2f31da6d28033294250a702
studentXxxxNagibatorxxX <arem125@yandex.ru>
2024-10-16 16:38:06 +0300

    up up up

--- Scenes/click_obj.gd
+++ Scenes/click_obj.gd
@@ -21,7 +21,8 @@ func _process(delta):
 	$ScoreLable.text = str(clik)
 	# информация лейбл
 	$"../info".text = "		ИНФО:
-сила клика = " + str(clikStr)
+сила клика = " + str(clikStr) + "
+пассивный доход = " + str(tikCoins)
 	# пассивный тик дохода
 	timer = timer + delta
 	# Проверяем, прошло ли 1 секунда
@@ -29,7 +30,13 @@ func _process(delta):
 		clik = clik + tikCoins  # Добавляем tikCoins к clik
 		timer = 0.0  # Сбрасываем таймер
 
+
+# кнопки обновление цены
+	$"../ButtonUpClik".text = "Улучшение нажатия\nцена: " + str(costUpgrade)
+	$"../ButtonUpPassiv".text = "Пассивный доход\nцена: " + str(costAutoCoin)
 	
+	
+
 # функция тапанья
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton and event.pressed and event.button_index != MOUSE_BUTTON_WHEEL_UP and event.button_index != MOUSE_BUTTON_WHEEL_DOWN:	
@@ -50,8 +57,7 @@ func _on_button_button_down() -> void:
 	if clik >= costUpgrade:
 		clik = clik - costUpgrade
 		# изменяем цену апгрейда
-		$"../Button".text = "Улучшение нажатия
-цена: " + str(costUpgrade)
+
 		costUpgrade = 100
 		# добавляем усиление клика 
 		clikStr = 2
@@ -60,8 +66,7 @@ func _on_button_button_down() -> void:
 func _on_button_2_button_down() -> void:
 	if clik >= costAutoCoin:
 		clik = clik - costAutoCoin
-		$"../Button2".text = "Пассивный доход
-цена: " + str(costAutoCoin)
+
 		costAutoCoin = costAutoCoin + 150
 		tikCoins = 1
 		costAutoCoin = 50
